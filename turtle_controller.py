47ad6754c0b89d7541c60eab3cc2171ecbebfeb1
Kavya <117969882+Kavya-812@users.noreply.github.com>
2022-11-19 12:32:14 +0530

    Add files via upload

---
+++ turtle_controller.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+import math
+import time
+  
+  
+class TurtleControllerNode(Node):
+    def __init__(self):
+        super().__init__("turtle_controller")
+        self.target_x = 8.0
+        self.target_y = 4.0
+        self.pose_ = None
+        self.cmd_vel_publisher_ = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
+        self.pose_subscriber_ = self.create_subscription(Pose, "turtle1/pose", self.callback_turtle_pose, 10)
+        self.control_loop_timer_ = self.create_timer(0.01, self.control_loop)
+    
+    def callback_turtle_pose(self,msg):
+        self.pose_ = msg
+        
+    def control_loop(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        self.cmd_vel_publisher_.publish(msg)
+        time.sleep(1)
+        msg.linear.y =1.0
+        self.cmd_vel_publisher_.publish(msg)
+        
+        msg.angular.z =(125*math.pi)/180
+        self.cmd_vel_publisher_.publish(msg)
+        time.sleep(1)
+        msg.angular.z = 1.0
+        self.cmd_vel_publisher_.publish(msg)
+
+     
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleControllerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+    
+       
+if __name__ == "__main__":
+	main()
